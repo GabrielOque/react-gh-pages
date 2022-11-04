8245f2d9e0d32b71cfde140eea7af54317555f89
Gabriel Oquendo <oquendogabriel18@gamil.com>
2022-11-04 12:15:01 -0500

    new

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <h1 className="w-full h-screen bg-red-500 flex justify-center">
-      <div className="w-40 h-40 bg-blue-600 mt-20">Hello world</div>
+    <h1 className="flex justify-center w-full h-screen bg-red-500">
+      <h>Hola papu nuevo deploy</h>
+      <div className="w-40 h-40 mt-20 bg-blue-600">Hello world</div>
     </h1>
   );
 }
