275078724f76d211a36d5407b35362f49541273a
Gabriel Oquendo <oquendogabriel18@gamil.com>
2022-11-06 23:57:19 -0500

    Add time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import Agregar from "./Agregar";
 import { useState, useEffect } from "react";
 import Quitar from "./Quitar";
 
+//
+
+function muestraReloj() {
+  var fechaHora = new Date();
+  var horas = fechaHora.getHours();
+  var minutos = fechaHora.getMinutes();
+  var segundos = fechaHora.getSeconds();
+
+  if (horas < 10) {
+    horas = "0" + horas;
+  }
+  if (minutos < 10) {
+    minutos = "0" + minutos;
+  }
+  if (segundos < 10) {
+    segundos = "0" + segundos;
+  }
+
+  document.getElementById("reloj").innerHTML =
+    horas + ":" + minutos + ":" + segundos;
+}
+
+window.onload = function () {
+  setInterval(muestraReloj, 1000);
+};
+
+//
+
 function App() {
   const [items, setItems] = useState(0);
 
@@ -29,6 +57,11 @@ function App() {
   return (
     <h1 className="flex justify-center w-full h-screen bg-red-500">
       <div className="mt-20 bg-blue-600 w-96 h-96 rounded-xl">
+        <div className="flex justify-center mt-2">
+          <div className="flex justify-center p-2 bg-red-700 rounded-full">
+            <p id="reloj" className="font-bold text-white"></p>
+          </div>
+        </div>
         <div className="flex">
           <div className="flex justify-center w-1/2">
             <Agregar items={items} setItems={setItems} />
@@ -66,8 +99,9 @@ function App() {
         m-0
         focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
       "
-              id="exampleFormControlInput1"
+              id="input"
               placeholder="Example label"
+              value={items + " " + "By Gabriel"}
             />
           </div>
         </div>
