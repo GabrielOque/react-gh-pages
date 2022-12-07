4b3d657d4ecc1a9c2649b0c1ffcf966d8688c2fd
Gabriel Oquendo <oquendogabriel18@gamil.com>
2022-12-06 21:49:48 -0500

    Add title header

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
     <div className="flex pl-6 pr-6 bg-blue-300 h-30">
       <div className="w-4/5 pt-4 pb-4">
         <p className="text-white">Agust 19, 2022</p>
-        <p className="text-2xl font-bold text-indigo-500 ">Good Day Gabriel!</p>
+        <p className="text-2xl font-bold text-indigo-500 ">
+          ¡Good Day Gabriel!
+        </p>
       </div>
       <div className="w-1/5">
         <div className="pt-2">
